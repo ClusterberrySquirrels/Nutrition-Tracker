14c75aba7478fd36e52cd0baa99a23ca67c1fbb7
Matt Hurt <mhurt@msudenver.edu>
2021-02-27 11:59:28 -0700

    Adding definition of getting the user id , load_user is undefined at the moment.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
@@ -11,3 +11,15 @@ def index():
 @app.route('/')
 def login():
     return render_template('login.html')
+
+
+def load_user(id):
+    pass
+
+
+@app.route('/user/<id>')
+def get_user(id):
+    user = load_user(id)
+    if not user:
+        abort(404)
+    return '<h1>Hello, {}</h1>'.format(user.name)
