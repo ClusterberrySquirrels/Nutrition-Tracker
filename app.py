18afe2aaab6f3a97e00cf430df9b43214b692ebc
Matt Hurt <mhurt@msudenver.edu>
2021-02-27 18:43:02 -0700

    Message flashing, form handling in view functions, and user sessions available

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -25,9 +25,17 @@ class NameForm(FlaskForm):
 #     return '<h1>Hello, {}</h1>'.format(user.name) # TODO: define name reference
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
 
 
 @app.errorhandler(500)
