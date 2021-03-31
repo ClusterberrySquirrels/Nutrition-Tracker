91a0be64d8ca1ca7becbf72209b486d9d273cedf
Noah <noahlaratta@gmail.com>
2021-03-31 16:18:36 -0600

    Updated tests, coverage at 98%

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
 from .. import create_app
+from flask_login import current_user
+from datetime import datetime
 
 def test_index(test_client):
     response = test_client.get('/')
@@ -16,6 +18,7 @@ def test_profile_logged_out(test_client):
 def test_page_not_found(test_client):
     response = test_client.get('/nonexistentpage')
     assert response.status_code == 404
+    
     
 #def test_internal_server_error(test_client):
 #    response = test_client.post('/login')
